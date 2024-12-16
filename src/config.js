a31c06b524c23fb1c6ccf2263320b8f0caabc2a2
briteming <briteming@gmail.com>
2024-12-15 21:36:57 -0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,26 +1,7 @@
 export default {
-  repo: 'blog', // required
-  owner: 'fouber', // required
-  title: 'fouber blog',
+  repo: 'sublog', // required
+  owner: 'briteming', // required
+  title: "brite's blog",
   token: '', // required. split your access_token with #, create in https://github.com/settings/tokens
-  perPage: '15',
-  social: [{
-    name: 'github',
-    url: ''
-  }, {
-    name: 'weibo',
-    url: ''
-  }, {
-    name: 'twitter',
-    url: ''
-  }, {
-    name: 'wechat',
-    url: ''
-  }, {
-    name: 'instagram',
-    url: ''
-  }, {
-    name: 'facebook',
-    url: ''
-  }]
+  perPage: '5'
 }
