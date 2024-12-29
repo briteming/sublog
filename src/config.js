1a8b66aecc8e54e31e43a96feeeae3f7413a598d
briteming <briteming@gmail.com>
2024-12-29 13:11:54 -0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export default {
   repo: 'sublog', // required
   owner: 'briteming', // required
   title: "brite's blog",
-  token: '', // required. split your access_token with #, create in https://github.com/settings/tokens
+  token: 'ghp_XNMeqyxAVPJoOYLS0nIH0n#NBC4Je8Q0oH1yH', // required. split your access_token with #, create in https://github.com/settings/tokens
   perPage: '5'
 }
